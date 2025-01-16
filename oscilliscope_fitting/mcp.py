1833c578cf49f7f46eb3da37eb5f136414bf26c3
Hayden Swanson <hayden.swanson@cern.ch>
2025-01-16 07:34:31 -0800

    removed the context manager for using dat2root

--- oscilliscope_fitting/mcp.py
+++ oscilliscope_fitting/mcp.py
@@ -87,11 +87,11 @@ class MCPSignalScaler:
         #return np.mean(base_x, axis=1)
 
     @classmethod
-    def normalize(cls, seconds: np.ndarray, volts: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    def normalize(cls, seconds: np.ndarray, volts: np.ndarray, signal_saturation_level:float=-0.52) -> tuple[np.ndarray, np.ndarray]:
         """
         Normalize signal to be between 0 and 1 by calculating the baseline and peak maximum!
+        It also removes any saturated signals by setting the arrays to np.NaN
         """
-
         # Gaurd Conditions
         if seconds.shape != volts.shape:
             raise ValueError(f"Time and voltage arrays should have the same shape, instead they have {seconds.shape}, {volts.shape} respectively.")
@@ -101,13 +101,11 @@ class MCPSignalScaler:
             seconds = np.array([seconds])
             volts = np.array([volts])
 
-        #seconds = cls._center_array(seconds)
-
         # REMOVE SATURATED SIGNALS
         # using np.where to preserve array length and np.min because the signal is negative \/
         # have to do [:,np.newaxis], just takes the array and wraps arrays around the inner values (float)
-        volts   = np.where(np.min(volts, axis=1)[:,np.newaxis] > SIGNAL_SATURATION_LEVEL, volts, np.NaN)
-        seconds = np.where(np.min(volts, axis=1)[:,np.newaxis] > SIGNAL_SATURATION_LEVEL, seconds, np.NaN)
+        volts   = np.where(np.min(volts, axis=1)[:,np.newaxis] > signal_saturation_level, volts, np.NaN)
+        seconds = np.where(np.min(volts, axis=1)[:,np.newaxis] > signal_saturation_level, seconds, np.NaN)
         #-----------------------------------------------------------------------------#
 
         peak_times, peak_volts = cls._calc_mcp_peaks(seconds, volts)
@@ -118,67 +116,3 @@ class MCPSignalScaler:
         volts_scaled = (volts - v_mins) / (v_maxs-v_mins)
 
         return seconds, volts_scaled
-
-
-class Dat2RootFit:
-    """
-    Dat2Root does not actually convert binary dat files to root, name is just legacy
-
-    This takes in a waveform, converts it to root file, then processes it using the compiled C++ file from TimingDAQ library
-    (will try to remove one day, all it does it perform fits on the MCP signal, and clock too but we do not use it because it is not reliable)
-
-    This context managers handles the deletion and creation of the files in case any bugs happen along the way!
-    """
-    def __init__(self, mcp_waveform: ak.Array, dat2root_path: str = None, config_path: str = None):
-        self.mcp_waveform = mcp_waveform
-
-        # keep input and output files in linux designated /tmp/ directory
-        self.input_path = "/tmp/input_dat2root_temp.root"
-        self.output_path = "/tmp/output_dat2root_temp.root"
-        self.dat2root_path = "oscilliscope_fitting/NetScopeStandalone_Dat2Root_MCP_FITTER" if dat2root_path is None else dat2root_path
-        self.config_path = "oscilliscope_fitting/LecroyScope_v12.config" if config_path is None else config_path
-
-    def __enter__(self):
-        # convert to root file for dat2root
-        self._convert_to_root(self.input_path)
-        # run dat2root
-        self._run_dat2root()
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        # delete both files
-        if os.path.exists(self.input_path):
-            os.remove(self.input_path)
-        if os.path.exists(self.output_path):
-            os.remove(self.output_path)
-
-    def _convert_to_root(self, path: str) -> None:
-        with uproot.recreate(path) as file:
-            file["pulse"] = self.mcp_waveform  # "mytree" is the name of the TTree
-
-    def _run_dat2root(self) -> None:
-        """
-        Creates root file from lecroy scope, does fitting on the MCP signal.
-        """
-        # /home/etl/Test_Stand/ETL_TestingDAQ/TimingDAQ/config/LecroyScope_v12.config
-        # Construct the command
-        DattorootCmd = (
-            f'{self.dat2root_path} '
-            f'--correctForTimeOffsets --input_file={self.input_path} '
-            f'--output_file={self.output_path} --config={self.config_path} --save_meas'
-        )
-        subprocess.run(DattorootCmd, shell=True, check=True)
-
-    def output(self) -> ak.Array:
-        with uproot.open(self.output_path) as file:
-            tree = file["pulse"]
-            data = tree.arrays(library="np")
-        return data
-
-def fit(mcp_waveform: ak.Array, **kwargs) -> ak.Array:
-    with Dat2RootFit(mcp_waveform, **kwargs) as fit_manager:
-        fit_data = fit_manager.output()
-
-        # take the fields you want and just return those
-
-        return fit_data
