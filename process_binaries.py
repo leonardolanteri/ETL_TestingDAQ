583023241e11a78a372768ed859a93b081e380fc
gekobs <giac.zecc@gmail.com>
2025-01-30 14:49:40 +0100

    new root structure that has the waveforms now, because of the move of the fitting to tbanalysis!

--- process_binaries.py
+++ process_binaries.py
@@ -21,10 +21,6 @@ from pathlib import Path
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-# CLOCK CONFIGURABLES
-# CLOCK_THRESH_LOW, CLOCK_THRESH_HIGH = 0.25, 0.8 #used to pick out the edges (between 0 and 1, percentage of the absolute amplitude)
-# CLOCK_MEAUREMENT_POINT = 0.5 #between 0 and 1, after the fit, where along the fitted y axis do we take the clock value
-# MCP_MAX_AMP = -1.35
 def consolidate_acquisition(output_file_path: str, etroc_binary_paths: list[str]=None, mcp_binary_path: str=None, clock_binary_path: str=None):
     t_file_reads = time.perf_counter()
 
@@ -33,41 +29,17 @@ def consolidate_acquisition(output_file_path: str, etroc_binary_paths: list[str]
     etroc_unpacked_data = etroc.converter(etroc_binary_paths, skip_trigger_check=True)
     etroc_data = etroc.root_dumper(etroc_unpacked_data) # root dumper name is due to history 
     if etroc_data is None:
-        # no etroc data...
         return
     
     logger.info(f"LOADING FILES TOOK {(time.perf_counter()-t_file_reads):.2f} seconds")
-    t_process_files = time.perf_counter()
 
-    # nanoseconds, scaled_volts = mcp.MCPSignalScaler.normalize(mcp_waveform.x * 1e9, mcp_waveform.y, signal_saturation_level=MCP_MAX_AMP)
-    # peak_times, peak_volts = mcp.MCPSignalScaler._calc_mcp_peaks(nanoseconds, mcp_waveform.y)
-    # mcp_timestamps = mcp.linear_interpolation(nanoseconds, scaled_volts, peak_times, threshold=0.4)
-    # clock_timestamps = clock.calc_clock(
-    #     ak.from_numpy(clock_waveform.x*1e9), ak.from_numpy(clock_waveform.y),
-    #     CLOCK_THRESH_LOW, CLOCK_THRESH_HIGH, CLOCK_MEAUREMENT_POINT
-    # )
-    logger.info(f"PROCESS FILES TOOK {(time.perf_counter()-t_process_files):.2f} seconds")
     t_write_files = time.perf_counter()
-
-    etroc_data_map = dict(zip(
-        ak.fields(etroc_data), ak.unzip(etroc_data)
-    ))
-
-    mcp_trigger_times, _ = mcp_waveform.segment_times
-    oscilliscope_merged_map = {
-        "i_evt": list(range(len(clock_waveform.x))),
-        "segment_time": mcp_trigger_times,
-        "mcp_volts": mcp_waveform.y,
-        "mcp_seconds": mcp_waveform.x,
-        "clock_volts": clock_waveform.y,
-        "clock_seconds": clock_waveform.x,
-    }
-
-    consolidated_array = etroc_data_map | oscilliscope_merged_map
-
     with uproot.recreate(output_file_path) as output:
-        output["pulse"] = consolidated_array
+        output["clock_waveforms"] = {'volts': clock_waveform.y, 'seconds': clock_waveform.x}
+        output["mcp_waveforms"]   = {'volts': mcp_waveform.y,   'seconds': mcp_waveform.x}
+        output["etroc_data"] = dict(zip(ak.fields(etroc_data), ak.unzip(etroc_data))) #silly uproot, need to unpack awk array
 
+    
     logger.info(f"WRITE FILE TOOK {(time.perf_counter()-t_write_files):.2f} seconds")
 
 
@@ -77,7 +49,7 @@ etroc_file = lambda run: Path(f"/home/etl/Test_Stand/module_test_sw/ETROC_output
 MCP_channel = 2
 CLK_channel = 3
 
-output_file_dir = Path(f"rereco_data2")
+output_file_dir = Path(f"rereco_data")
 output_file_dir.parent.mkdir(parents=True, exist_ok=True)
 output_file = lambda run: Path(f"run_{run}.root")
 
@@ -91,7 +63,6 @@ def consolidate_acquisition_task(run):
         etroc_binary_paths=[etroc_file(run)],
         mcp_binary_path=trace_file(MCP_channel, run),
         clock_binary_path=trace_file(CLK_channel, run),
-        oscilliscope_reference_path=trace_file(MCP_channel, run)
     )
 
 t_consolidated = time.perf_counter()
@@ -99,8 +70,8 @@ with Pool() as pool:
     results = pool.imap_unordered(
         consolidate_acquisition_task, range(run_start, run_stop + 1)
     )
-
     for _ in results:
         ...
 
 print(f"COMPLETED IN: {(time.perf_counter()-t_consolidated):.2f} seconds")
+
