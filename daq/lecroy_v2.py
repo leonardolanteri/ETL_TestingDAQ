695d4f916803f69b27d30a6f3fd3a70b4ca4fd9e
gekobs <giac.zecc@gmail.com>
2025-02-06 18:41:34 +0100

    where I left off, fixed the offset and range calculation for vertical axis, working on horizontal axis now!

--- daq/lecroy_v2.py
+++ daq/lecroy_v2.py
@@ -31,7 +31,7 @@ class RunLecroy:
         self.num_vertical_divs = 8
         self.num_horizontal_divs = 10
 
-    def vertical_setup(self, channel: int, start: float, stop: float) -> None:
+    def vertical_setup(self, channel: int, lower_bound: float=-1, upper_bound: float=1) -> None:
         """
         Specifies the set up of the vertical axis for the oscillisope. The start and 
         stop arguments specify the range of voltage values for the vertical axis.
@@ -41,14 +41,14 @@ class RunLecroy:
         start: Lower bound value in Volts for the vertical axis.
         stop: Upper bound value in Volts for the vertical axis.
         """   
-        volts_div = (stop - start) / self.num_vertical_divs
-        print(f"Setting channel {channel} to {volts_div} V/div")
-
+        volts_div = (upper_bound - lower_bound) / self.num_vertical_divs
+        offset = (upper_bound+lower_bound)/2
+        print(f"Setting channel {channel} to {volts_div} V/div with an offset of {offset}")
         self.lecroy.write(f"C{channel}:COUPLING D50")
         self.lecroy.write(f"C{channel}:VOLT_DIV {volts_div}V")
-        self.lecroy.write(f"C{channel}:OFFSET {0}V")
+        self.lecroy.write(f"C{channel}:OFFSET {offset}V")
 
-    def horizontal_setup(self, start:float, stop: float) -> None:
+    def horizontal_setup(self, lower_bound:float=-25e-9, upper_bound: float=25e-9) -> None:
         """
         Specifies the set up of the horizontal axis for the oscillisope. The start and 
         stop arguments specify the range of time values for the horizontal axis.
@@ -58,8 +58,11 @@ class RunLecroy:
         start: Lower bound value in Seconds for the horizontal axis.
         stop: Upper bound value in Seconds for the horizontal axis.
         """
-        time_div = (stop-start)/self.num_horizontal_divs
+        time_div = (upper_bound-lower_bound)/self.num_horizontal_divs
         self.lecroy.write(f"TIME_DIV {time_div}S")
+        self.lecroy.write(f"TRIG_DELAY")
+    def sample_rate(self, value):
+        ...
 
     def __repr__(self):
         """
@@ -76,4 +79,5 @@ class RunLecroy:
         return '\n'.join(f"{name} = {value}" for name, value in zip(names, idn))
 
 lecroy = RunLecroy("192.168.0.6")
-lecroy.vertical_setup(2, -1, 1.5)
+lecroy.vertical_setup(2, lower_bound=-2, upper_bound=0.5)
+lecroy.horizontal_setup(2, -2, 0.5)
