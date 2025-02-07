6aa2e7ba466a29067202f68c471453099126086d
gekobs <giac.zecc@gmail.com>
2025-02-07 12:01:48 +0100

    moved each channel into its own class to have a better name space

--- daq/lecroy_v2.py
+++ daq/lecroy_v2.py
@@ -6,61 +6,106 @@ import datetime
 import pyvisa as visa
 import glob
 import pdb
+from typing import Literal
 
-class RunLecroy:
-    """
-    This is the manual for all of the commands:
-    https://cdn.teledynelecroy.com/files/manuals/wr2_rcm_revb.pdf
-    """
-    def __init__(self, ip_address: str):
-        # Set Up Connection to Oscilliscope
-        rm = visa.ResourceManager("@py")
-        self.lecroy = rm.open_resource(f'TCPIP0::{ip_address}::INSTR')
-        self.lecroy.timeout = 3000000
-        self.lecroy.encoding = 'latin_1'
-        self.lecroy.clear()
-
-        self.lecroy.write('STOP') #Stops any acquisition processes
-        self.lecroy.write("*CLS") #not sure what this does
-
-        # This makes it so the scope just returns the number and not extra information
-        self.lecroy.write("COMM_HEADER OFF")
-        self.lecroy.write("BANDWIDTH_LIMIT OFF")
-
+class Channel:
+    def __init__(self, channel:int, lecroy_connection: visa.resources.Resource):
+        self.number = channel
+        self.lecroy_conn = lecroy_connection
         # This is standard for oscilliscopes (number of boxes on the screen in vert and horz direction)
         self.num_vertical_divs = 8
-        self.num_horizontal_divs = 10
+        self.is_trigger_channel = False
 
-    def vertical_setup(self, channel: int, lower_bound: float=-1, upper_bound: float=1) -> None:
+    def vertical_axis(self, lower_bound:float, upper_bound:float, units:Literal['V', 'MV']='V'):
         """
         Specifies the set up of the vertical axis for the oscillisope. The start and 
         stop arguments specify the range of voltage values for the vertical axis.
 
         Arguments
         channel: Oscillsicope channel 
-        start: Lower bound value in Volts for the vertical axis.
-        stop: Upper bound value in Volts for the vertical axis.
-        """   
+        lower_bound, upper_bound: Specify the lower and upper bounds of the vertical axis
+        """
         volts_div = (upper_bound - lower_bound) / self.num_vertical_divs
         offset = (upper_bound+lower_bound)/2
-        print(f"Setting channel {channel} to {volts_div} V/div with an offset of {offset}")
-        self.lecroy.write(f"C{channel}:COUPLING D50")
-        self.lecroy.write(f"C{channel}:VOLT_DIV {volts_div}V")
-        self.lecroy.write(f"C{channel}:OFFSET {offset}V")
+        self.volts_div(volts_div, units=units)
+        self.vertical_offset(offset, units=units)
 
-    def horizontal_setup(self, lower_bound:float=-25e-9, upper_bound: float=25e-9) -> None:
+    def volts_div(self, volts_div:float, units:Literal['V', 'MV'] = 'V'):
+        """Set volts per div for a channel"""
+        self.lecroy_conn.write(f"C{self.number}:VOLT_DIV {volts_div}{units}")
+    
+    def vertical_offset(self, offset:float, units:Literal['V', 'MV'] = 'V'):
+        """Sets the vertical offset"""
+        self.lecroy_conn.write(f"C{self.number}:OFFSET {offset}{units}")
+
+    def coupling(self, coupling:Literal['D50']):
+        self.lecroy_conn.write(f"C{self.number}:COUPLING {coupling}")
+
+class Lecroy:
+    """
+    This is the manual for all of the commands:
+    https://cdn.teledynelecroy.com/files/manuals/wr2_rcm_revb.pdf
+    """
+    def __init__(self, ip_address: str, active_channels:list[int] = None):
+        # Set Up Connection to Oscilliscope
+        rm = visa.ResourceManager("@py")
+        self.lecroy_conn = rm.open_resource(f'TCPIP0::{ip_address}::INSTR')
+        self.lecroy_conn.timeout = 3000000
+        self.lecroy_conn.encoding = 'latin_1'
+        self.lecroy_conn.clear()
+
+        self.lecroy_conn.write('STOP') #Stops any acquisition processes
+        self.lecroy_conn.write("*CLS") #not sure what this does
+
+        # This makes it so the scope just returns the number and not extra information
+        self.lecroy_conn.write("COMM_HEADER OFF")
+        self.lecroy_conn.write("BANDWIDTH_LIMIT OFF")
+
+        self.active_channels = [1,2,3,4] if active_channels is None else active_channels
+        self.channels = {chnl: Channel(chnl) for chnl in active_channels}        
+
+        self.num_horizontal_divs = 10
+
+    def horizontal_axis(self, lower_bound: float, upper_bound:float, units:Literal['S', 'NS', 'US', 'MS', 'KS'] = 'NS') -> None:
         """
         Specifies the set up of the horizontal axis for the oscillisope. The start and 
         stop arguments specify the range of time values for the horizontal axis.
+        NS for nanoseconds, US for microseconds, MS for milliseconds, S for seconds, or KS for kiloseconds.
 
         Arguments
-        channel: Oscillsicope channel 
-        start: Lower bound value in Seconds for the horizontal axis.
-        stop: Upper bound value in Seconds for the horizontal axis.
+        lower_bound, upper_bound: Specify the lower and upper bounds of the time axis
+
+        TODO: Test the offest makes sense being trigger delay
         """
+        offset = (upper_bound+lower_bound)/2
         time_div = (upper_bound-lower_bound)/self.num_horizontal_divs
-        self.lecroy.write(f"TIME_DIV {time_div}S")
-        self.lecroy.write(f"TRIG_DELAY")
+        self.time_div(time_div, units=units)
+        self.trigger_delay(offset, units=units)
+
+    def time_div(self, time_div:float=None, units:Literal['S', 'NS', 'US', 'MS', 'KS'] = 'S'):
+        """
+        Set time per div for a channel
+        NS for nanoseconds, US for microseconds, MS for milliseconds, S for seconds, or KS for kiloseconds.
+        """
+        self.lecroy_conn.write(f"TIME_DIV {time_div}{units}")
+    
+    def trigger_delay(self, delay:float, units:Literal['S', 'NS', 'US', 'MS', 'KS'] = 'S'):
+        """
+        Set trigger delay
+        NS for nanoseconds, US for microseconds, MS for milliseconds, S for seconds, or KS for kiloseconds.
+        """
+        self.lecroy_conn.write(f"TRIG_DELAY {delay}{units}")
+    
+    def trigger_slope(self, slope:Literal["POS", "NEG", "WINDOW"]):
+        self.lecroy_conn.write(f"TRIG_SLOPE {slope}")
+
+    def trigger_select(self, channel:Channel, trigger_type:Literal["DROP", "EDGE", "GLIT", "INTV", "STD", "SNG", "SQ", "TEQ"]=None):
+        """Select what channel to use as trigger."""
+        
+        assert all(not chnl.is_trigger_channel for chnl in self.channels.values()), "For simplicity, only channel can be desiginated as the trigger channel"
+        channel.is_trigger_channel = True
+        self.lecroy_conn.write(f"TRIG_SELECT {trigger_type},SR,{channel.number}")
+    
     def sample_rate(self, value):
         ...
 
@@ -72,12 +117,18 @@ class RunLecroy:
         manufacturer, the scope model, the serial
         number and the firmware revision.
         """
-        idn = self.lecroy.query("*IDN?").split(',')
+        idn = self.lecroy_conn.query("*IDN?").split(',')
         names = ['manufacturer', 'scope model', 'serial number', 'firmware version']
         if len(idn) != len(names):
             raise ValueError("Unexpected response format")
         return '\n'.join(f"{name} = {value}" for name, value in zip(names, idn))
 
-lecroy = RunLecroy("192.168.0.6")
-lecroy.vertical_setup(2, lower_bound=-2, upper_bound=0.5)
-lecroy.horizontal_setup(2, -2, 0.5)
+
+lecroy = Lecroy("192.168.0.6", active_channels=[2,3])
+
+lecroy.channels[2].coupling(coupling='D50')
+lecroy.channels[3].vertical_axis(-2,2, units='V')
+
+lecroy.horizontal_axis(-2, 0.5, units='NS')
+
+lecroy.trigger_select(lecroy.channels[2], trigger_type="EDGE")
